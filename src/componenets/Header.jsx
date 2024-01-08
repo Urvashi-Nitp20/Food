666fa6892e30f5c786f26a8bb95beec5a38d2a40
Urvashi-Nitp20 <discordhackslash@gmail.com>
2024-01-08 23:09:46 +0530

    Continued Header

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './Header.css';
 function Header() {
    return (
-    <>
-    <img src='./logo.png' className='logo'/>
-    <img src='./pizza.png' width={735} height={804} className='pizza'/>
-    <img src='./Vector.png' width={752} height={839} className='vector'/>
-    
-    </>
-   ) 
+      <>
+      <img src='./logo.png' className='logo' />
+         <div className='head'>
+            <div className='heading'>
+               <h1 className='heading1'>
+                  Discover the<br />
+                  Best Food <br />and Drinks
+               </h1>
+
+               <p>Naturally made Healthcare Products for the <br/>better care & support of your body.</p>
+            </div>
+            <div>
+               
+               <img src='./pizza.png' className='pizza' />
+               <img src='./Vector.png' className='vector' />
+            </div>
+         </div>
+      </>
+   )
 }
 export default Header
